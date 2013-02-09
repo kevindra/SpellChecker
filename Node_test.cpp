abd66cc552e2334cad4a244943b3468dc7a607b5
Kevindra Singh <kevindra.singh@gmail.com>
2013-02-09 22:39:58 +0530

    removing test cases

--- Node_test.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * Node_test.cpp
- *
- *  Created on: 09-Feb-2013
- *      Author: Kevindra
- */
-
-#include "Node.h"
-
--- SpellChecker_test.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * SpellChecker_test.cpp
- *
- *  Created on: 09-Feb-2013
- *      Author: Kevindra
- */
-
-#include "SpellChecker.h"
-
